63cbe91debb7b5e044c66cce82c9a486c4c06c2a
JACKKK7 <59097255+trishaaldians@users.noreply.github.com>
2021-04-10 14:39:54 +0700

    Update Main.h

--- Common/Main.h
+++ Common/Main.h
@@ -1,23 +1,10 @@
 #pragma once
 #include <windows.h>
-#include <string>
 #include <string.h>
-#include <vector>
-#include <intrin.h>
 #include <sstream>
-#include <fstream>
-#include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
-#include <winioctl.h>
-#include <Iphlpapi.h>
-#include <time.h>
 #include <iomanip>
 #include <direct.h>
-#include <memory>
-#include <algorithm>
-#include <map>
 
 #pragma warning(disable : 4996 4244)
 
